f20614764f6eecc24d712cc61a39d32b52685091
Diego Cuenca <dcuenca@lorente.fin.ec>
2023-03-17 10:42:14 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de Empleados';
+  cuadroNombre: string = '';
+  cuadroApellido: string = '';
+  cuadroCargo: string = '';
+  cuadroSalario: number = 0;
+
+  empleados: Empleado[] = [
+    new Empleado('Diego', 'Cuenca', 'Analista Desarrollador', 680),
+    new Empleado('Silvana', 'Jiménez', 'Banca Personas', 800),
+    new Empleado('Juan', 'Hidalgo', 'Jefe Desarrollo', 1500),
+    new Empleado('Carlos', 'Cuenca', 'Jefe Riesgos', 1500)
+  ];
+
+  agregarEmpleado() {
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+    this.clearForm();
+  }
+
+  clearForm() {
+    this.cuadroNombre = '';
+    this.cuadroApellido = '';
+    this.cuadroCargo= '';
+    this.cuadroSalario = 0;
+  }
+
 }
---
+++ src/app/empleado.model.ts
@@ -0,0 +1,13 @@
+export class Empleado {
+  nombre: string;
+  apellido: string;
+  cargo: string;
+  salario: number;
+
+  constructor(nombre: string, apellido: string, cargo: string, salario: number) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.cargo = cargo;
+    this.salario = salario;
+  }
+}
